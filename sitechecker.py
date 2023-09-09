7f7e7355f2c18604d2a927a48df7058f46bcf7d5
Victor Holz <vholz@salesforce.com>
2023-09-09 08:10:08 +0000

    Updating sitechecker

--- sitechecker.py
+++ sitechecker.py
@@ -42,11 +42,9 @@ class SiteChecker(object):
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
         
-        self.service = Service(executable_path = shutil.which("chromedriver"))
-        self.driver = webdriver.Chrome(self.service, options = options)
+        service = Service(executable_path = shutil.which("chromedriver"))
+        self.driver = webdriver.Chrome(service, options = options)
         self.driver.implicitly_wait(1)
         self.driver.set_window_position(0, 0)
         self.driver.set_window_size(1920, 1080)
