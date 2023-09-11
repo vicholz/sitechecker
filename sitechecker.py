25811845f3882b37451badb73c1c8d7215570d99
Victor Holz <vholz@salesforce.com>
2023-09-11 07:09:57 +0000

    Updating options

--- sitechecker.py
+++ sitechecker.py
@@ -37,10 +37,6 @@ class SiteChecker(object):
         user_agent = ua.random
         options = webdriver.ChromeOptions()
         options.add_argument(f'--user-agent="{self.data.get("properties").get("useragent")}"')
-        options.add_argument("--disable-blink-features")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument('--disable-gpu')
-        options.add_argument('--no-sandbox')
         options.add_argument('--headless')
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
